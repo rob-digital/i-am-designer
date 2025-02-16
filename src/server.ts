63f33c607961ea542557e115873e330648162998
rob-digital <robert.roksela@gmail.com>
2025-02-16 14:50:00 +0000

    netlify package added to package.json

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import {
   isMainModule,
   writeResponseToNodeResponse,
 } from '@angular/ssr/node';
+
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
