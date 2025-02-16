0cf823ca425d68c4cf8fdb38082ea80f24cdfa90
rob-digital <robert.roksela@gmail.com>
2025-02-16 17:13:22 +0000

    removin uncesary pages

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
-import { AppLayout } from './app/layout/component/app.layout';
-import { Dashboard } from './app/pages/dashboard/dashboard';
-import { Documentation } from './app/pages/documentation/documentation';
-import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
 import { Layout0Component } from './app/layout/component/layout-0/layout-0.component';
 import { Content0Component } from './app/pages/content/content-0.component';
@@ -14,12 +10,9 @@ export const appRoutes: Routes = [
         children: [
             { path: '', component: Content0Component},
             { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-            { path: 'documentation', component: Documentation },
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
         ]
     },
-    { path: 'landing', component: Landing },
     { path: 'notfound', component: Notfound },
-    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
 ];
